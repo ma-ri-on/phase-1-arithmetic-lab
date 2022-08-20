bc9b12caed340c55928dd0a4861b5c9bbb259db8
Marion <marionasukuku@gmail.com>
2022-08-20 15:39:54 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply =num1*num2;
+
+const random = Math.floor(Math.random() * num2)+1;
+
+const num3 = 14;
+const num4 = 5;
+
+const mod = num3%num4;
+
+const max = Math.max(12, 14, 20)
